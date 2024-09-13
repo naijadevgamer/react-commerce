264c807eca133ba503155e6601157835f91c15a5
thenaijagamer <sabdullahialaba50@gmail.com>
2024-09-13 18:26:00 +0100

    Add the BrandShowcase component Style and add marquee effect animation

--- src/components/BrandShowcase.tsx
+++ src/components/BrandShowcase.tsx
@@ -7,8 +7,8 @@ import zara from "@/assets/zara.svg";
 
 const BrandShowcase = () => {
   return (
-    <div className="w-full my-20 flex items-center relative mr-20">
-      <div className="marquee-inner flex justify-between w-full absolute mr-8 duration-1000">
+    <div className="relative my-20 mr-20 flex w-[90rem] items-center">
+      <div className="marquee-inner absolute mr-8 flex w-full justify-between duration-1000">
         <img className="logo" src={gucci} alt="Gucci logo" />
         <img className="logo" src={levi} alt="Levi logo" />
         <img className="logo" src={underArmour} alt="Under Armour logo" />
@@ -16,7 +16,7 @@ const BrandShowcase = () => {
         <img className="logo" src={adidas} alt="Adidas logo" />
         <img className="logo" src={zara} alt="Zara logo" />
       </div>
-      <div className="marquee-inner flex justify-between w-full absolute -right-[109%] duration-1000">
+      <div className="marquee-inner absolute -right-[109%] flex w-full justify-between duration-1000">
         <img className="logo" src={gucci} alt="Gucci logo" />
         <img className="logo" src={levi} alt="Levi logo" />
         <img className="logo" src={underArmour} alt="Under Armour logo" />
