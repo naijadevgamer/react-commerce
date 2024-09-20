c5cf8d671decba7a8cd2fdd636f8aafe69e297ce
thenaijagamer <sabdullahialaba50@gmail.com>
2024-09-20 01:21:11 +0100

    Import new components for display And make some minor adjustment

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,16 @@ import vid from "@/assets/video.mp4";
 import vidWeb from "@/assets/video.mp4";
 import BrandShowcase from "@/components/BrandShowcase";
 import Categories from "@/components/Categories";
+import NewArrivals from "@/components/NewArrivals";
+import FAQ from "@/components/FAQ";
 
 const HomePage = () => {
   return (
-    <main className="px-14 lg:px-20 overflow-hidden">
+    <main className="overflow-hidden px-8 md:px-12 lg:px-20">
       {/* Display Section*/}
-      <section className="w-full text-white h-[40rem] rounded-2xl relative text-accent-foreground-foreground overflow-hidden">
+      <section className="text-accent-foreground-foreground relative h-[40rem] w-full overflow-hidden rounded-2xl text-white">
         {/* Backgroud*/}
-        <div className="absolute top-0 left-0 h-full w-full brightness-50 overflow-hidden">
+        <div className="absolute left-0 top-0 h-full w-full overflow-hidden brightness-50">
           <video className="h-full w-full object-cover" autoPlay muted loop>
             <source src={vid} type="video/mp4" />
             <source src={vidWeb} type="video/webm" />
@@ -18,15 +20,15 @@ const HomePage = () => {
           </video>
         </div>
         {/* Texts */}
-        <div className="px-10 lg:px-16 absolute bottom-0 w-full">
-          <h1 className="capitalize text-center scroll-m-20 text-5xl mb-16 font-semibold tracking-tight lg:text-7xl">
+        <div className="absolute bottom-0 w-full px-10 lg:px-16">
+          <h1 className="font-audiowide mb-16 scroll-m-20 text-center text-5xl font-semibold capitalize tracking-tight lg:text-7xl">
             Elevate your elegance, elevate your style
           </h1>
-          <div className="flex flex-col text-center sm:text-start gap-5 sm:gap-0 sm:flex-row items-center justify-between mb-16">
-            <h3 className="text-xl lg:text-3xl max-w-[40rem] w-full sm:w-1/2 ">
+          <div className="mb-16 flex flex-col items-center justify-between gap-5 text-center sm:flex-row sm:gap-0 sm:text-start">
+            <h3 className="font-montserratAlt w-full max-w-[40rem] text-xl sm:w-1/2 lg:text-3xl">
               Discover timeless trends and express your unique style journey
             </h3>
-            <Button className="rounded-3xl" asChild>
+            <Button className="font-audiowide rounded-3xl" asChild>
               <a href="/login">Explore now</a>
             </Button>
           </div>
@@ -34,6 +36,8 @@ const HomePage = () => {
       </section>
       <BrandShowcase />
       <Categories />
+      <NewArrivals />
+      <FAQ />
     </main>
   );
 };
