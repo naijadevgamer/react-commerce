0b5032639e786e656952453d7a1e11f97eb562f0
thenaijagamer <sabdullahialaba50@gmail.com>
2024-09-20 01:17:27 +0100

    Add accordion from shadcn and configure font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,86 +3,108 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        "large-desktop": "1800px", // 1800px
-        // tl: "1200px", // tablet-landscape
-        // tp: "900px", // tablet-portrait
-        // sp: "300px", // small-phone
-      },
-      animation: {
-        go: "go 0.3s cubic-bezier(1, 0, 0, 1) both",
-        goBack: "goBack 0.3s cubic-bezier(1, 0, 0, 1) 0.6s both",
-      },
-      keyframes: {
-        go: {
-          from: { transform: "translateX(0%)" },
-          to: { transform: "translateX(105%)" },
-        },
-      },
-      transitionDuration: {
-        image: "6s",
-      },
-      gridTemplateColumns: {
-        category: "1.5fr repeat(2, 1fr)",
-        "body-tp": "10fr, 15fr",
-        days: "repeat(auto-fit, minmax(13rem, 1fr))",
-        // column: "",
-      },
-      gridTemplateRows: {
-        category: "repeat(7, 1fr)",
-        body: "minmax(102.3rem, 100vh)",
-        "body-tl": "minmax(max-content, 100vh)",
-        "body-p": "minmax(100vh, max-content), max-content",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
-          3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
-        },
-      },
-    },
+  	extend: {
+  		screens: {
+  			'large-desktop': '1800px'
+  		},
+  		animation: {
+  			go: 'go 0.3s cubic-bezier(1, 0, 0, 1) both',
+  			goBack: 'goBack 0.3s cubic-bezier(1, 0, 0, 1) 0.6s both',
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		fontFamily: {
+  			audiowide: 'Audiowide, sans-serif',
+  			montserratAlt: 'Montserrat Alternates, sans-serif'
+  		},
+  		keyframes: {
+  			go: {
+  				from: {
+  					transform: 'translateX(0%)'
+  				},
+  				to: {
+  					transform: 'translateX(105%)'
+  				}
+  			},
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		transitionDuration: {
+  			image: '3s'
+  		},
+  		gridTemplateColumns: {
+  			category: '1.5fr 0.5fr 1.5fr',
+  			'body-tp': '10fr, 15fr',
+  			days: 'repeat(auto-fit, minmax(13rem, 1fr))'
+  		},
+  		gridTemplateRows: {
+  			category: 'repeat(7, 1fr)',
+  			body: 'minmax(102.3rem, 100vh)',
+  			'body-tl': 'minmax(max-content, 100vh)',
+  			'body-p': 'minmax(100vh, max-content), max-content'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
