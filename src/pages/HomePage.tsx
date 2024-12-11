c54bbac15d32c595daa11e4140de781c2685d509
thenaijagamer <sabdullahialaba50@gmail.com>
2024-12-11 01:30:59 +0100

    Remove footer and nav from the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,17 @@
+import Heroes from "@/components/Heroes";
 import BrandShowcase from "@/components/BrandShowcase";
 import Categories from "@/components/Categories";
 import NewArrivals from "@/components/NewArrivals";
 import FAQ from "@/components/FAQ";
-import Heroes from "@/components/Heroes";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 
 const HomePage = () => {
   return (
     <main className="overflow-hidden">
-      <Navbar />
       <Heroes />
       <BrandShowcase />
       <Categories />
       <NewArrivals />
       <FAQ />
-      <Footer />
     </main>
   );
 };
