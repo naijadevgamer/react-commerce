2daf36b2bcd4f09111ee0ebbda04c3593d5f2cca
thenaijagamer <sabdullahialaba50@gmail.com>
2025-01-08 03:18:26 +0100

    Implement optional redirect for login and sign up pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,64 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import ShopPage from "./pages/ShopPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import store from "./Redux/store.ts";
 import ConnectedApp from "./App.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import ConnectedCartPage from "./pages/CartPage.tsx";
+import { RootState } from "./interfaces.ts";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ConnectedApp />, // Use Layout as the parent route
-    children: [
-      { path: "/", element: <HomePage /> },
-      {
-        path: "shop",
-        element: <ShopPage />,
-        children: [
-          {
-            path: ":itemId",
-            element: <ShopPage />,
-          },
-        ],
-      },
-      { path: "cart", element: <ConnectedCartPage /> },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "signup",
-        element: <RegisterPage />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <div>Not Found</div>, // Fallback for unmatched routes
-  },
-]);
+const AppRouter = () => {
+  const currentUser = useSelector((state: RootState) => state.user.currentUser);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <ConnectedApp />, // Use Layout as the parent route
+      children: [
+        { path: "/", element: <HomePage /> },
+        {
+          path: "shop",
+          element: <ShopPage />,
+          children: [
+            {
+              path: ":itemId",
+              element: <ShopPage />,
+            },
+          ],
+        },
+        { path: "cart", element: <ConnectedCartPage /> },
+        {
+          path: "login",
+          element: currentUser ? <Navigate to="/" replace /> : <LoginPage />,
+        },
+        {
+          path: "signup",
+          element: currentUser ? <Navigate to="/" replace /> : <RegisterPage />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <div>Not Found</div>, // Fallback for unmatched routes
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <AppRouter />
     </Provider>
   </StrictMode>,
 );
