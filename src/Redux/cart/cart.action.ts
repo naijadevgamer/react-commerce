ff542779942edc49fb4562a0187a48cbee479875
thenaijagamer <sabdullahialaba50@gmail.com>
2025-01-08 15:18:38 +0100

    Add increament, remove, and decreament actions

--- src/Redux/cart/cart.action.ts
+++ src/Redux/cart/cart.action.ts
@@ -1,6 +1,22 @@
+import { Item } from "@/interfaces";
 import { cartActionTypes } from "./cart.types";
 
-export const setCartItems = (item: any) => ({
+export const addItemToCart = (item: Item) => ({
   type: cartActionTypes.ADD_ITEM,
+  payload: item,
+});
+
+export const incrementItemQuantity = (item: Item) => ({
+  type: cartActionTypes.INCREASE_ITEM_QUANTITY,
+  payload: item,
+});
+
+export const decrementItemQuantity = (item: Item) => ({
+  type: cartActionTypes.DECREASE_ITEM_QUANTITY,
+  payload: item,
+});
+
+export const removeItemFromCart = (item: Item) => ({
+  type: cartActionTypes.REMOVE_ITEM,
   payload: item,
 });
