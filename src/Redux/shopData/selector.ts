2ae2183001a455a7434778d9e877e19e63f8855f
thenaijagamer <sabdullahialaba50@gmail.com>
2025-01-10 23:54:18 +0100

    Add collection seclector

--- src/Redux/shopData/selector.ts
+++ src/Redux/shopData/selector.ts
@@ -1,9 +1,25 @@
 import { RootState } from "@/interfaces";
 import { createSelector } from "@reduxjs/toolkit";
 
+const urlIdConnector: Record<string, number> = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
 const shop = (state: RootState) => state.shop;
 
 export const selectShopData = createSelector(
   [shop],
   (selector) => selector.shopData,
 );
+
+export const selectCollection = (url: string) =>
+  createSelector([selectShopData], (collections) => {
+    console.log(collections, url);
+    return collections.find(
+      (collection) => collection.id === urlIdConnector[url],
+    );
+  });
